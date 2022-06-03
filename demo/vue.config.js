4a6c49e4784a32976fd269aae93df11df78f49af
kricn <mnyt2436@126.com>
2022-06-03 21:39:22 +0800

    添加 elementui 自定义主题

--- demo/vue.config.js
+++ demo/vue.config.js
@@ -10,12 +10,12 @@ module.exports = {
   },
   css: {
     loaderOptions: {
-      // sass: {
-      //   //依次导入的公用的scss变量，公用的scss混入，共用的默认样式
-      //   additionalData: `
-      //     @import "@/styles/color.scss";
-      //   `,
-      // }
+      sass: {
+        //依次导入的公用的scss变量，公用的scss混入，共用的默认样式
+        additionalData: `
+          @import "@/styles/element-ui.scss";
+        `,
+      }
     },
   },
   configureWebpack: {
