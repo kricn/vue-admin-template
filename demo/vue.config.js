bddc3cf0042e1722655e726f64f2b0635eb41746
kricn <mnyt2436@126.com>
2022-06-06 21:49:10 +0800

    自动引入element-icon

--- demo/vue.config.js
+++ demo/vue.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
+const Icons = require('unplugin-icons/webpack')
+const IconsResolver = require('unplugin-icons/resolver')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = {
   pages: {
@@ -21,10 +23,24 @@ module.exports = {
   configureWebpack: {
     plugins: [
       AutoImport({
-        resolvers: [ElementPlusResolver()],
+        resolvers: [
+          ElementPlusResolver(), 
+          IconsResolver({
+            prefix: 'Icon',
+          })
+        ],
       }),
       Components({
-        resolvers: [ElementPlusResolver()],
+        resolvers: [
+          ElementPlusResolver(),
+          IconsResolver({
+            enabledCollections: ['ep'],
+          }),
+        ],
+      }),
+      Icons({
+        autoInstall: true,
+        compiler: 'vue3'
       }),
     ],
   },
